0ab9ecc17fd18e8c72caef801873e25a8c3a514d
Dudsssssss <105220552+Dudsssssss@users.noreply.github.com>
2023-11-17 09:18:00 -0300

    a

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function tocasom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeTeclas =document.querySelectorAll(".tecla");
 
 let contador = 0;
 while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas [contador].classList[1];
+    const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
         tocasom (idAudio)
-    }
-    contdor = contador +1;
+}
+    contador = contador +1;
     console.log(contador);
 }
